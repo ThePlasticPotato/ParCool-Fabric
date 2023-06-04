8cde25ae682b0b1302659c1f3b1fecce8478930e
ThePlasticPotato <77798144+ThePlasticPotato@users.noreply.github.com>
2023-06-04 12:34:29 -0400

    Fabric Setup (Nothing ported yet)

--- src/main/java/com/alrex/parcool/proxy/CommonProxy.java
+++
@@ -1,7 +0,0 @@
-package com.alrex.parcool.proxy;
-
-import net.minecraftforge.network.simple.SimpleChannel;
-
-public abstract class CommonProxy {
-	public abstract void registerMessages(SimpleChannel instance);
-}
